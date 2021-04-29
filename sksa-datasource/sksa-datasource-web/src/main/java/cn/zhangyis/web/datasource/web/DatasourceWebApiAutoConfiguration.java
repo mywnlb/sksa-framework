8cdba71311a1aa3b47aa398318c5902ddf714ce3
623178903 <623178903@qq.com>
2021-04-30 00:07:15 +0800

    初始化提交pom

---
+++ sksa-datasource/sksa-datasource-web/src/main/java/cn/zhangyis/web/datasource/web/DatasourceWebApiAutoConfiguration.java
@@ -0,0 +1,9 @@
+package cn.zhangyis.web.datasource.web;
+
+/**
+ * @Description 多数据源web配置  spring。factore自动加载
+ * @Date 2021/4/29 23:30
+ * @Created by lb
+ */
+public class DatasourceWebApiAutoConfiguration {
+}
